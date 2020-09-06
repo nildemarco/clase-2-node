b58315b8a43ab4a6a752c8cc4ef6d96a3d69c383
nildemarco <nildemarco@gmail.com>
2020-09-06 17:59:27 -0300

    add post, delete, get and put methods

--- app.js
+++ app.js
@@ -1,28 +1,113 @@
+const fs = require('fs');
 const express = require('express');
-
+const { restart } = require('nodemon');
 const app = express();
 
 const port = 8080;
+
+app.use(express.json());
 
 // app.get('/', ((req, res) => {
 //     res.send("Hola desde express")
 // }))
 
-app.get('/', ((req, res) => {
-    res.json({
-        estado: "tu pedido fue exitoso!",
-    })
+app.get('/gatitos', ((req, res) => {
+    fs.readFile(`${__dirname}/assets/gatitos.txt`, ((err, data) => {
+
+        dataJSON = JSON.parse(data);
+
+        res.json({
+            status: 'success',
+            data: dataJSON,
+        })
+        if (err) {
+            res.sendStatus(500).send("Algo anda mal");
+        }
+    }))
 }))
 
 
-app.post('/hola', ((req, res) => {
-    // res.send("Hiciste un post a la ruta hola");
+app.post('/gatitos', ((req, res) => {
+    let nuevoGatitoAGuardar = req.body;
 
-    res.status(200).json({
-        estado: "Tu post fue exitoso",
-    })
+    if (nuevoGatitoAGuardar) {
+        fs.readFile(`${__dirname}/assets/gatitos.txt`, ((err, data) => {
+            const dataJSON = JSON.parse(data);
+            nuevoGatitoAGuardar.id = dataJSON.length;
+
+            dataJSON.push(nuevoGatitoAGuardar)
+
+            fs.writeFile(`${__dirname}/assets/gatitos.txt`, JSON.stringify(dataJSON), (err) => {
+                res.status(201).json({
+                    estado: "success",
+                    data: {
+                        nuevoGatitoAGuardar,
+                        createAt: new Date(),
+                    }
+                })
+            })
+        }))
+    }
+}))
+
+app.delete('/gatitos/:id', ((req, res) => {
+    fs.readFile(`${__dirname}/assets/gatitos.txt`, ((err, data) => {
+        const dataJSON = JSON.parse(data);
+        idGatitoABorrar = Number(req.params.id);
+
+        const arrGatitosFiltrados = dataJSON.filter(gato => gato.id !== idGatitoABorrar);
+
+        fs.writeFile(`${__dirname}/assets/gatitos.txt`, JSON.stringify(arrGatitosFiltrados), (err) => {
+            res.status(202).json({
+                status: 'success',
+                data: arrGatitosFiltrados,
+            })
+        })
+    }))
+
+}))
+
+
+app.put('/gatitos/:id', ((req, res) => {
+    fs.readFile(`${__dirname}/assets/gatitos.txt`, ((err, data) => {
+        const dataJSON = JSON.parse(data);
+        const idGatitoAEditar = Number(req.params.id);
+        const nuevoGatito = req.body;
+
+        let gatoEncontrado = dataJSON.find(gato => gato.id === idGatitoAEditar)
+
+        if (gatoEncontrado) {
+            let gatoActualizado = {
+                id: gatoEncontrado.id,
+                name: nuevoGatito.name,
+                shortDesc: nuevoGatito.shortDesc,
+                longDesc: nuevoGatito.longDesc,
+                img: nuevoGatito.img,
+                colores: nuevoGatito.colores,
+                sexo: nuevoGatito.sexo,
+                disponible: nuevoGatito.disponible,
+            }
+            let targetIndex = dataJSON.indexOf(gatoEncontrado);
+
+            dataJSON.splice(targetIndex, 1, gatoActualizado);
+
+            fs.writeFile(`${__dirname}/assets/gatitos.txt`, JSON.stringify(dataJSON), (err) => {
+                res.status(201).json({
+                    status: 'success',
+                    data: dataJSON
+                });
+            })
+        } else {
+            res.sendStatus(404);
+        }
+
+
+
+
+    }))
 }))
 
 app.listen(port, () => {
     console.log(`App esta corriendo en el puerto ${port}`);
 })
+
